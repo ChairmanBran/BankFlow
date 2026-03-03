6b44197b39f3ef46f3b53b85038f57aa81ccce84
chairmanbran <54674688+ChairmanBran@users.noreply.github.com>
2026-03-04 00:33:52 +0800

    fix some bugs

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from utils import PdfCutter, PdfClaudeAsker, DataToExcel, JsonToExcelRowValue
 from configs import Config
 
 def main(pdf_routine):
-    dir_name = Config().paths["pdf_dir"]
+    dir_name = Config().get("paths.pdf_dir")
     cutter = PdfCutter(pdf_routine, dir_name)
     cutter.cut_pdf()
     asker = PdfClaudeAsker()
@@ -18,7 +18,7 @@ def main(pdf_routine):
     for bank_statement in all_json_list:
         json_dealer.deal_single_json(bank_statement)
     headers = json_dealer.headers
-    save_routine = Config().paths["excel_dir"] + Config().excel["default_filename"]
+    save_routine = Config().get("paths.excel_dir") + Config().get("excel.default_filename")
     data = json_dealer.operate_lists
     excel_data = DataToExcel(data=data, save_routine=save_routine, headers=headers)
     done, do_info = excel_data.save_xl()
